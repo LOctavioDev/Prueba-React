9251dfcced69a7cde5e7e3b69da07d7998da6d5f
Luis Octavio <141202473+LOctavioDev@users.noreply.github.com>
2024-05-06 14:19:07 -0600

    Fix: Se separó la aplicación por componentes

---
+++ src/components/Img.js
@@ -0,0 +1,12 @@
+export default function Img() {
+  return (
+    <>
+      <div className="image">
+        <img
+          src="https://el4tocaarmine.github.io/Layout/img/cookie4.jpg"
+          alt="Imagen de Pacman"
+        ></img>
+      </div>
+    </>
+  );
+}
---
+++ src/components/WidgetF.js
@@ -0,0 +1,16 @@
+export default function () {
+  return (
+    <>
+      <div className="widget">
+        <h3 className="title">Informacion</h3>
+        <p>
+          Observamos que partimos de la hoja izquierda en este caso 5 luego
+          subimos en la inclinación derecha el 6 pero no iremos a la raíz así q
+          iremos a la hoja 9 de ahí a la dos y como no se puede subir mas y
+          mantener la inclinación hacia la derecha pasamos a la hoja 11 y como
+          completamos el lado izquierdo iremos a la raíz el 2
+        </p>
+      </div>
+    </>
+  );
+}
